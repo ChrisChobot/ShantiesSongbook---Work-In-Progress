0dc60d07344ac8842019404879ab281e0825e436
Christoph Chobot <a@a.a>
2021-11-22 02:08:56 +0100

    typescript nuget + deleted unused directives

--- ShantiesSongbook/DataAccess/ShantiesSeeder.cs
+++ ShantiesSongbook/DataAccess/ShantiesSeeder.cs
@@ -1,11 +1,5 @@
 ﻿using DataAccess.Entities;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess
 {
